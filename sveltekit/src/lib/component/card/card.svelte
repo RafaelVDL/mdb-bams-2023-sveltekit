5daacc71dbe774e9911936126a34ded1ecc3dda1
Mark Daniel Bandilla <markbandilla.dpwm@yahoo.com>
2023-05-26 21:50:12 +0800

    Add Card Component

---
+++ sveltekit/src/lib/component/card/card.svelte
@@ -0,0 +1,15 @@
+<script>
+    import CardHeader from '$lib/component/card/header.svelte';
+    import CardBody from '$lib/component/card/body.svelte';
+</script>
+
+<div class="card">
+    <slot>
+        <CardHeader>
+            <h2 class="h4">Card Header</h2>
+        </CardHeader>
+        <CardBody>
+            <p>Card Body</p>
+        </CardBody>
+    </slot>
+</div>
