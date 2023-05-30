748ccf1a2e7896c813f076bdb812863ec7303b9b
Mark Daniel Bandilla <markbandilla.dpwm@yahoo.com>
2023-05-30 14:42:32 +0800

    Add Auth Store, Update Navbar & Sidebar

--- sveltekit/src/lib/markup/navbar.svelte
+++ sveltekit/src/lib/markup/navbar.svelte
@@ -1,18 +1,33 @@
 <script>
-    import imgSrc from '$lib/image/pdp_logo.png';
+    import { PUBLIC_APP_NAME, PUBLIC_BARANGAY_LOGO } from "$env/static/public";
+    import { page } from '$app/stores';
+    import { auth } from '$lib/module/auth/store.js';
+    import UiIcon from '$lib/component/ui/icon.svelte';
+
+    const login = () => {
+        auth.set({
+            loggedIn: true,
+            photo: "https://bams.ortadeltech.com/assets/images/profile.png",
+            fullname: "Firstname M.I. Lastname",
+            profession: "Profession"
+        });
+    }
+    const logout = () => {
+        auth.set({});
+    }
 </script>
 
 <header class="d-flex w-100 border-bottom py-2">
     <a href="/"
         class="nav-brand d-none flex-shrink-0 text-decoration-none d-flex ms-3 d-md-flex flex-column flex-md-row align-items-center"
         style="width: 280px;">
-        <img src={imgSrc} width="75" alt="logo" />
-        <span class="display-6 ps-4 text-uppercase" style="margin-top: -5px;">BAMS</span>
+        <img src={PUBLIC_BARANGAY_LOGO} width="75" alt="logo" />
+        <span class="display-6 ps-4 text-uppercase" style="margin-top: -5px;">{PUBLIC_APP_NAME}</span>
     </a>
     <a href="/"
         class="nav-brand d-md-none flex-shrink-0 text-decoration-none d-flex flex-column flex-md-row align-items-center justify-content-center"
         style="width: 100px;">
-        <img src={imgSrc} width="75" alt="logo" />
+        <img src={PUBLIC_BARANGAY_LOGO} width="75" alt="logo" />
     </a>
     <nav class="navbar flex-grow-1">
         <ul class="nav w-100 align-items-center">
@@ -23,22 +38,25 @@
                 </a>
             </li>
             <li class="nav-item">
-                <h1 class="h3">Page Title</h1>
+                <h1 class="h3 text-capitalize">{$page.route.id?.substring(1).replace("/", " / ")}</h1>
             </li>
-            <li class="ms-auto nav-item">
-                <a class="nav-link" href="#">
-                    <img class="rounded-circle shadow-sm" src="https://bams.ortadeltech.com/assets/images/profile.png"
-                        width="50" alt="profile image link">
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="#">Separated link</a></li>
-                </ul>
+            <li class="ms-auto nav-item dropdown">
+                {#if $auth.loggedIn}
+                    <a class="nav-link dropdown-toggle" href="#" 
+                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img class="rounded-circle shadow-sm" src={$auth.photo}
+                            width="50" alt="profile image link">
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#" on:click|preventDefault={logout}>Log-out</a></li>
+                    </ul>
+                {:else}
+                    <button class="btn btn-primary me-3" on:click={login}><UiIcon name="right-to-bracket" type="solid"></UiIcon> Log-in</button>
+                {/if}
             </li>
             <li class="nav-item">
             </li>
--- sveltekit/src/lib/markup/sidebar.svelte
+++ sveltekit/src/lib/markup/sidebar.svelte
@@ -1,25 +1,34 @@
+<script>
+    import { auth } from '$lib/module/auth/store.js';
+    import UiIcon from '$lib/component/ui/icon.svelte';
+    let links = [
+        {title: "People", url: "./people", icon: {name: "users", type: ""}},
+        {title: "Animals", url: "./animals", icon: {name: "paw", type: "solid"}}
+    ]
+</script>
+
 <!-- Sidebar -->
 <aside id="sidebar" class="collapse collapse-horizontal show border-end h-100">
-    <div class="content" style="width: 300px;">
+    <div class="content" style="width: 280px;">
         <!-- Profile -->
-        <div class="profile d-flex align-items-center border-bottom">
-            <img class="rounded-3 m-3 shadow-sm" src="https://bams.ortadeltech.com/assets/images/profile.png"
-                width="80" alt="profile image">
-            <span class="ms-2">
-                <b class="name lead lh-sm">Firstname M.I. Lastname</b>
-                <p class="title m-0">Title</p>
-            </span>
-        </div>
+        {#if $auth.loggedIn}
+            <div class="profile d-flex align-items-center border-bottom">
+                <img class="rounded-3 m-3 shadow-sm" src={$auth.photo} width="80" alt="profile image">
+                <span class="ms-2">
+                    <b class="name lead lh-sm">{$auth.fullname}</b>
+                    <p class="title m-0">{$auth.profession}</p>
+                </span>
+            </div>
+        {/if}
         <nav class="list-group">
             <ul class="nav">
+                {#each links as link}
                 <li class="nav-item w-100">
-                    <a href="./people" class="nav-link active p-3 border-bottom" aria-current="true">
-                        <i class="fa fa-users px-2"></i> &nbsp;People
-                    </a>
-                    <a href="./animals" class="nav-link active p-3 border-bottom" aria-current="true">
-                        <i class="fa fa-users px-2"></i> &nbsp;Animals
+                    <a href={link.url} class="nav-link active p-3 border-bottom" aria-current="true">
+                        <UiIcon name={link.icon.name} type={link.icon.type}></UiIcon> &nbsp;{link.title}
                     </a>
                 </li>
+                {/each}
             </ul>
         </nav>
     </div>
