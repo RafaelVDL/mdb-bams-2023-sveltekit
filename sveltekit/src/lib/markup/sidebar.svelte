cd720e04a4b4613b723858ae5574e3d72ac31516
Mark Daniel Bandilla <markbandilla.dpwm@yahoo.com>
2023-06-02 14:20:01 +0800

    Update Sidebar UI

--- sveltekit/src/lib/markup/sidebar.svelte
+++ sveltekit/src/lib/markup/sidebar.svelte
@@ -8,11 +8,11 @@
 </script>
 
 <!-- Sidebar -->
-<aside id="sidebar" class="collapse collapse-horizontal show border-end h-100">
-    <div class="content" style="width: 280px;">
+<aside id="sidebar" class="collapse collapse-horizontal show border-end h-100 overflow-hidden flex-shrink-0">
+    <div class="content d-flex flex-column h-100 overflow-hidden" style="width: 280px;">
         <!-- Profile -->
         {#if $auth.loggedIn}
-            <div class="profile d-flex align-items-center border-bottom">
+            <div class="profile d-flex flex-shrink-0 align-items-center border-bottom">
                 <img class="rounded-3 m-3 shadow-sm" src={$auth.photo} width="80" alt="profile image">
                 <span class="ms-2">
                     <b class="name lead lh-sm">{$auth.fullname}</b>
@@ -20,16 +20,19 @@
                 </span>
             </div>
         {/if}
-        <nav class="list-group">
-            <ul class="nav">
-                {#each links as link}
-                <li class="nav-item w-100">
-                    <a href={link.url} class="nav-link active p-3 border-bottom" aria-current="true">
-                        <UiIcon name={link.icon.name} type={link.icon.type}></UiIcon> &nbsp;{link.title}
-                    </a>
-                </li>
-                {/each}
-            </ul>
-        </nav>
+        <div class="flex-grow-1 overflow-y-auto">
+            <nav class="list-group">
+                <ul class="nav">
+                    {#each links as link}
+                    <li class="nav-item w-100">
+                        <a href="/admin/{link.url}" class="nav-link active p-3 border-bottom" aria-current="true">
+                            <UiIcon name={link.icon.name} type={link.icon.type}></UiIcon> &nbsp;{link.title}
+                        </a>
+                    </li>
+                    {/each}
+                </ul>
+            </nav>
+        </div>
+        <p class="text-center text-body-secondary m-0 py-2 border-top">© 2023 Company, Inc</p>
     </div>
 </aside>
