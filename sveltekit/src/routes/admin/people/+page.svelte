bfae7ea70939ed2ce4d78f08b5ab34657814f367
Mark Daniel Bandilla <markbandilla.dpwm@yahoo.com>
2023-06-02 14:25:19 +0800

    Update People Page & Dynamic View Page

--- sveltekit/src/routes/admin/people/+page.svelte
+++ sveltekit/src/routes/admin/people/+page.svelte
@@ -1,8 +1,5 @@
 <script>
     import ModulePeople from '$lib/module/people/people.svelte';
-    import ModulePeopleUiModal from '$lib/module/people/ui/modal.svelte';
 </script>
 
-<ModulePeople></ModulePeople>
-<ModulePeopleUiModal></ModulePeopleUiModal>
-    
+<ModulePeople></ModulePeople>    
