bf29c2045bd05e9126e61b1e835255f79a33e9d6
Mark Daniel Bandilla <markbandilla.dpwm@yahoo.com>
2023-06-02 14:20:41 +0800

    Add Methods to Auth Store

--- sveltekit/src/lib/module/auth/store.js
+++ sveltekit/src/lib/module/auth/store.js
@@ -1,3 +1,21 @@
 import { writable } from 'svelte/store';
+const {set, update, subscribe} = writable({});
 
-export const auth = writable({});
+const authStore = () => {
+    return {
+        subscribe,
+        login: () => {
+            set({
+                loggedIn: true,
+                photo: "https://bams.ortadeltech.com/assets/images/profile.png",
+                fullname: "Firstname M.I. Lastname",
+                profession: "Profession"
+            });
+        },
+        logout: () => {
+            set({});
+        }
+    }
+}
+
+export const auth = authStore();
